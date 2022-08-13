f62ff09cb8e350b3dc8273e03bb826e408e9d658
Ulia Kamalova <mysula@mail.ru>
2022-08-13 20:40:37 +0500

    Задача 6

---
+++ 6 Task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ( " Введите число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+if( firstNumber % 2 ==0 )
+{
+   Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
